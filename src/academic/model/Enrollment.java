63504724fbd15d7c1622390629deee42f1f04e52
PaulaTambunan <paulatambunan10@gmail.com>
2026-03-03 09:24:45 +0700

    Enrollment.java

--- src/academic/model/Enrollment.java
+++ src/academic/model/Enrollment.java
@@ -1,11 +1,51 @@
 package academic.model;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S24025-Paula Gevriella Tambunan
  */
 public class Enrollment {
+    private String courseCode;   // Diganti dari objek Course
+    private String studentId;    // Diganti dari objek Student
+    private String academicYear; // Atribut baru
+    private String semester;     // Atribut baru
+    private String grade;        // Akan kita default menjadi "None"
 
-    // class definition
+    public Enrollment(String courseCode, String studentId, String academicYear, String semester) {
+        this.courseCode = courseCode;
+        this.studentId = studentId;
+        this.academicYear = academicYear;
+        this.semester = semester;
+        this.grade = "None"; // Menginisialisasi grade secara default menjadi "None"
+    }
 
+    // Getters untuk semua atribut
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getAcademicYear() {
+        return academicYear;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    // Metode untuk mendapatkan detail Enrollment dalam format output yang diminta
+    public String getEnrollmentDetails() {
+        return courseCode + "|" + studentId + "|" + academicYear + "|" + semester + "|" + grade;
+    }
+
+    @Override
+    public String toString() {
+        return getEnrollmentDetails();
+    }
 }
