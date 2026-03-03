17f9094c1e0c0433998254956ce1edb02a42ab2c
PaulaTambunan <paulatambunan10@gmail.com>
2026-03-03 09:24:26 +0700

    Course.java

--- src/academic/model/Course.java
+++ src/academic/model/Course.java
@@ -1,11 +1,44 @@
 package academic.model;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S24025-Paula Gevriella Tambunan
  */
 public class Course {
+    private String code;
+    private String name;
+    private int credits;
+    private String grade; // Assuming 'C' or 'D' from input example is a default grade for the course or an indicator
 
-    // class definition
+    public Course(String code, String name, int credits, String grade) {
+        this.code = code;
+        this.name = name;
+        this.credits = credits;
+        this.grade = grade;
+    }
 
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    // Method to get course details in the specified output format
+    public String getCourseDetails() {
+        return code + "|" + name + "|" + credits + "|" + grade;
+    }
+
+    @Override
+    public String toString() {
+        return getCourseDetails();
+    }
 }
